fb9b46939b2c14f6094918af0bc912a3cf0135d8
SandeepBhandari <sandeep.bhandari@almasons.com>
2025-06-11 16:29:30 +0530

    only white theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <link
           rel="preload"
           href="/bandeins.woff2"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        marqueeReverse: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-      },
-      animation: {
-        marquee: "marquee 40s linear infinite",
-        marqueeReverse: "marqueeReverse 40s linear infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
